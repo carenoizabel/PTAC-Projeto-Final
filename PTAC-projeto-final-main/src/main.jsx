6c7b00998e3fd0596311c979bcad8241bcbe2d24
carenoizabel <izabelcareno12345@gmail.com>
2023-11-28 00:55:00 -0400

    Criei pagina Musica para ser a pagina de musicas  ouvidas recentemente
    Nessa parte só criei a pasta e importei no meu main.jsx

--- PTAC-projeto-final-main/src/main.jsx
+++ PTAC-projeto-final-main/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home/Home.jsx'
 import Register from './Register/Register.jsx'
+import Musica from './Musica/Musica.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/musica" element={<Musica />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
