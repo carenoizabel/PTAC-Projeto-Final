c52fc2216f3fac200ebb7d4f819232ba7616f48b
carenoizabel <izabelcareno12345@gmail.com>
2023-12-08 14:24:13 -0400

    Página Inicial
    Só o layout mesmo, coloquei o componente menu, não quer carregar meus icons não sei pq, tentar resolver com o professor

--- PTAC-projeto-final-main/src/Componentes/Menu.jsx
+++ PTAC-projeto-final-main/src/Componentes/Menu.jsx
@@ -5,21 +5,21 @@ import '@fortawesome/fontawesome-free/css/all.css';
 
 export default function Menu () {
     return(
-  <div class="navbar">
-  <div class="logo">
+  <div className="navbar">
+  <div className="logo">
     <img src="Spotify_Logo_RGB_White.png" alt="" />
   </div>
-  <ul class="menu">
-    <li><a href="#"><i class="fas fa-home"></i> Início</a></li>
-    <li><a href="#"><i class="fas fa-search"></i> Buscar</a></li>
-    <li><a href="#"><i class="fas fa-book-open"></i> Sua Biblioteca</a></li>
+  <ul className="menu">
+    <li><a href="#"><i className="fas fa-home"></i> Início</a></li>
+    <li><a href="#"><i className="fas fa-search"></i> Buscar</a></li>
+    <li><a href="#"><i className="fas fa-book-open"></i> Sua Biblioteca</a></li>
   </ul>
-  <div class="playlist">
+  <div className="playlist">
     <h3>Playlists</h3>
     <ul>
       <li><Link to = {"/"}><img src="albumtaylor.jpeg" alt="" className="albumplay" />   Minha Playlist</Link></li>
-      <li><a href="#"><i class="fas fa-heart"></i> Últimas Escutadas</a></li>
-      <li><a href="#"><i class="fas fa-plus"></i> Adicionar a Playlist</a></li>
+      <li><a href="#"><i className="fas fa-heart"></i> Últimas Escutadas</a></li>
+      <li><a href="#"><i className="fas fa-plus"></i> Adicionar a Playlist</a></li>
     </ul>
   </div>
 </div>
--- PTAC-projeto-final-main/src/Home/Home.jsx
+++ PTAC-projeto-final-main/src/Home/Home.jsx
@@ -1,8 +1,22 @@
 import Menu from "../Componentes/Menu.jsx";
-export default function Home(){
-    return(
-        <div>
-            <Menu/>
+import "./home.css";
+import '@fortawesome/fontawesome-free/css/all.css';
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="navbar">
+                <Menu />
+            </div>
+            <div className="main">
+                <div className="usuario">
+                     <img className ="img-user" src="imguser.png" alt="" />
+                     <p>Olá, Izabel</p>
+                </div>
+                <div className="config">
+                    <p><i className="fa-regular fa-bell"></i></p>
+                    <p><i className="fa-solid fa-users"></i></p>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
