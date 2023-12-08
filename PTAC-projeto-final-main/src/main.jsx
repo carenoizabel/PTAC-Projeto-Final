ebab05bc63c996ce3400d511b910d0dd40040930
carenoizabel <izabelcareno12345@gmail.com>
2023-12-08 17:30:51 -0400

    import nova rota
    importei uma nova rota musica, tentei importar a destaque porém esta dando erro, vou fazer primeiro essa e depois tento de novo

--- PTAC-projeto-final-main/src/main.jsx
+++ PTAC-projeto-final-main/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home/Home.jsx'
 import Register from './Register/Register.jsx'
 import Menu from './Componentes/Menu.jsx'
+import Musica from './Componentes/Musica.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,7 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/componentes" element={<Menu />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
+        <Route path="/musica" element={<Musica />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
