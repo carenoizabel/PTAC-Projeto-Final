54c81ed230283ed63251a3b65eb2500bb9be3c9b
carenoizabel <izabelcareno12345@gmail.com>
2023-12-10 04:32:04 -0400

    Apagando rotas
    Não necessário criar rotas, pq é "pai para filho"

--- PTAC-projeto-final-main/src/main.jsx
+++ PTAC-projeto-final-main/src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home/Home.jsx'
 import Register from './Register/Register.jsx'
-import Menu from './Componentes/Menu.jsx'
 import Musica from './Componentes/Musica.jsx'
-import MusicaInd from './Componentes/MusicaInd.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,9 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/menu" element={<Menu />}></Route>
         <Route path="/musica" element={<Musica />}></Route>
-        <Route path="/musicaind" element={<MusicaInd />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
